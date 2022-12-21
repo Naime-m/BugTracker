b22779a9628d7c1dc01d6479eede538f5edf512c
Naime Medjek <naimemedjek@gmail.com>
2022-12-21 20:34:03 +0100

    added db context

---
+++ BugTracker/Data/BugTrackerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BugTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BugTracker.Data;
+
+public class BugTrackerDbContext : DbContext
+{
+    public BugTrackerDbContext(DbContextOptions options) : base(options)
+    { }
+
+    public DbSet<Bug> Bugs { get; set; }
+}
