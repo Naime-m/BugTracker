700c5ae25fc83bec3a73cbff459466ffe928e623
Naime Medjek <naimemedjek@gmail.com>
2022-12-21 21:36:51 +0100

    added validation and style in create cshtml

--- BugTracker/Models/Bug.cs
+++ BugTracker/Models/Bug.cs
@@ -1,9 +1,15 @@
-﻿namespace BugTracker.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models;
 
 public class Bug
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; } = string.Empty;
+    [Required]
+    [StringLength(300)]
     public string Description { get; set; } = string.Empty;
     public string Author { get; set; } = string.Empty; 
 
