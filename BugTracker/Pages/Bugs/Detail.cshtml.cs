a21633c70859b17e77bf9c364fcb772f32a6a38c
Naime Medjek <naimemedjek@gmail.com>
2022-12-22 15:09:03 +0100

    created detail razor page

---
+++ BugTracker/Pages/Bugs/Detail.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugTracker.Pages.Bugs;
+
+public class DetailModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
