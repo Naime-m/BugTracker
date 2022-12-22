c4e7619397a326291e5dd43f957ce1e87987fd11
Naime Medjek <naimemedjek@gmail.com>
2022-12-22 15:10:24 +0100

    added dbcontext to detailmodel constructor

--- BugTracker/Pages/Bugs/Detail.cshtml.cs
+++ BugTracker/Pages/Bugs/Detail.cshtml.cs
@@ -1,3 +1,4 @@
+using BugTracker.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,12 @@ namespace BugTracker.Pages.Bugs;
 
 public class DetailModel : PageModel
 {
+    private readonly BugTrackerDbContext _context;
+
+    public DetailModel(BugTrackerDbContext context)
+    {
+        _context = context;
+    }
     public void OnGet()
     {
     }
