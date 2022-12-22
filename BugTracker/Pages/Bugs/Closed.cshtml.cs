67ef1f11880ffadd2371c9ac52a621e023774efb
Naime Medjek <naimemedjek@gmail.com>
2022-12-22 19:00:43 +0100

    added closed pages and dbcontext to it

---
+++ BugTracker/Pages/Bugs/Closed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugTracker.Pages.Bugs
+{
+    public class ClosedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
