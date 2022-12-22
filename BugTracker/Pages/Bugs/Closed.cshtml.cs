8844110cf574d66d9bc44615e13ce5e93d213b32
Naime Medjek <naimemedjek@gmail.com>
2022-12-22 19:11:34 +0100

    added styling and closed bugs section

--- BugTracker/Pages/Bugs/Closed.cshtml.cs
+++ BugTracker/Pages/Bugs/Closed.cshtml.cs
@@ -1,12 +1,24 @@
+using BugTracker.Data;
+using BugTracker.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTracker.Pages.Bugs
 {
     public class ClosedModel : PageModel
     {
-        public void OnGet()
+        private readonly BugTrackerDbContext _context;
+
+        public ClosedModel(BugTrackerDbContext context)
         {
+            _context = context;
+        }
+        public IEnumerable<Bug> Bugs { get; set; } = Enumerable.Empty<Bug>();
+
+        public async Task OnGet()
+        {
+            Bugs = await _context.Bugs.Where(b => b.Status == Status.Closed).ToListAsync();
         }
     }
 }
